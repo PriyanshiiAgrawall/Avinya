fdd0ed9bc66edb28107a12454092f35a8e41a5a1
Priyanshi Agrawal <priyanshiprofessional1@gmail.com>
2025-05-11 23:15:48 +0530

    fixed nextjs middleware

--- MediSetu/frontend/src/middleware.js
+++ MediSetu/frontend/src/middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log("🚀 ~ middleware ~ request:", request);
-  // Middleware logic here
+
   return NextResponse.next();
 }
